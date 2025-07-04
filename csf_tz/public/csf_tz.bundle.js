23d753bd7c7ca50556e892597db2475445f73950
iphenelist <innocentphenelist@gmail.com>
2025-07-04 10:16:09 +0300

    refactor: module imports to use frappe.require and renaming csfUtils.js to csftz_utils.js utility functions

--- csf_tz/public/csf_tz.bundle.js
+++ csf_tz/public/csf_tz.bundle.js
@@ -1,6 +1,9 @@
 // csf_tz/public/csf_tz.bundle.js
 
-import "./js/select_dialog.js";
-import "./js/to_console.js";
-import "./js/jobcards/jobcards.js";
-import "vuetify/dist/vuetify.js";
+frappe.require([
+  '/assets/csf_tz/js/select_dialog.js',
+  '/assets/csf_tz/js/to_console.js',
+  '/assets/csf_tz/js/csftz_utils.js',
+  '/assets/csf_tz/js/jobcards/jobcards.js',
+  '/assets/csf_tz/node_modules/vuetify/dist/vuetify.js'
+]);
