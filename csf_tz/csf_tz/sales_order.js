622ab24e8e021659b411f342bb170616900d3fc6
iphenelist <innocentphenelist@gmail.com>
2025-07-18 12:07:56 +0300

    fix: Replace custom getValue/getDoc/getList with standard Frappe functions

--- csf_tz/csf_tz/sales_order.js
+++ csf_tz/csf_tz/sales_order.js
@@ -1,12 +1,12 @@
 frappe.require([
-    '/assets/csf_tz/js/csfUtlis.js',
     '/assets/csf_tz/js/shortcuts.js'
 ]);
 
 frappe.ui.form.on("Sales Order", {
     refresh: function (frm) {
-        frm.csf_settings = getDoc("CSF TZ Settings", "CSF TZ Settings");
-        if (frm.csf_settings.limit_uom_as_item_uom == 1) {
+        frappe.get_doc("CSF TZ Settings", "CSF TZ Settings").then(csf_settings => {
+            frm.csf_settings = csf_settings;
+            if (frm.csf_settings.limit_uom_as_item_uom == 1) {
             frm.set_query("uom", "items", function (frm, cdt, cdn) {
                 let row = locals[cdt][cdn];
                 return {
@@ -18,7 +18,8 @@ frappe.ui.form.on("Sales Order", {
                     },
                 };
             });
-        }
+            }
+        });
     },
     customer: function (frm) {
         if (!frm.doc.customer) {
