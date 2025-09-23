69eb16f502d94243a2157450dc1842da265966ea
iphenelist <innocentphenelist@gmail.com>
2025-09-23 11:58:40 +0300

    fix: renaming of the patch

--- csf_tz/patches/delete_versions_for_vehicle_fine_record_and_parking_bill.py
+++
@@ -1,8 +0,0 @@
-import frappe
-
-def execute():
-    for doctype in ["Vehicle Fine Record", "Parking Bill"]:
-        versions = frappe.get_all("Version", filters={"ref_doctype": doctype}, pluck="name")
-        for version_name in versions:
-            frappe.delete_doc("Version", version_name, force=1)
-    frappe.db.commit()
