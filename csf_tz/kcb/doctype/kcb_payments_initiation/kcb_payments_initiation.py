fd2542fef8ec6c652b53d67a02714aa1f8169ac6
Sydney Kibanga <skibanga@aakvatech.com>
2026-02-15 14:04:06 +0300

    fix(kcb): hardcode gpg binary path

--- csf_tz/kcb/doctype/kcb_payments_initiation/kcb_payments_initiation.py
+++ csf_tz/kcb/doctype/kcb_payments_initiation/kcb_payments_initiation.py
@@ -36,7 +36,8 @@ class KCBPaymentsInitiation(Document):
 
         gpg_home = frappe.get_site_path("private", "kcb_gpg")
         os.makedirs(gpg_home, exist_ok=True)
-        gpg = gnupg.GPG(home=gpg_home, options=["--pinentry-mode", "loopback"])
+        os.environ["GNUPGHOME"] = gpg_home
+        gpg = gnupg.GPG(gpgbinary="/usr/bin/gpg", options=["--pinentry-mode", "loopback"])
         public_key_path = frappe.get_site_path("private", "files", "kcb_public_key.asc")
         if not os.path.exists(public_key_path):
             frappe.throw("KCB public key not found at private/files/kcb_public_key.asc")
